eb150760529bc0d336a02a6a3147289a0377e452
Shojib13 <36606111+Shojib13@users.noreply.github.com>
2020-04-01 09:02:46 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,37 @@
+package com.httpsshojibbhuiyan.stopcorona;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView= (WebView) findViewById(R.id.webviewid);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://corona.shojibbhuiyan.com/");
+    }
+
+
+
+}
